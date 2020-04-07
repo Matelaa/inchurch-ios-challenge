1d2039f30085ef8ca4427a24147374cc0092578c
Jose Matela <jmatelaneto@gmail.com>
2020-04-06 22:23:10 -0300

    Initial Commit

---
+++ inchurch-ios-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inchurch-ios-challenge
+//
+//  Created by matela on 06/04/20.
+//  Copyright © 2020 José Matela Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
