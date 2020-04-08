8c94143e01ba3b03eb2586523b6936705298b9f3
Jose Matela <jmatelaneto@gmail.com>
2020-04-08 13:19:14 -0300

    final design collectionview and your cell

---
+++ inchurch-ios-challenge/Controllers/Home/Cell/MovieCollectionViewCell.swift
@@ -0,0 +1,103 @@
+//
+//  MovieCollectionViewCell.swift
+//  inchurch-ios-challenge
+//
+//  Created by matela on 08/04/20.
+//  Copyright © 2020 José Matela Neto. All rights reserved.
+//
+
+import UIKit
+import Reusable
+
+class MovieCollectionViewCell: UICollectionViewCell, Reusable {
+    
+    private let view: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let pictureMovie: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let bottomView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let nameMovie: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configureCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func bind() {
+        self.nameMovie.text = "Teste"
+    }
+    
+    private func configureCell() {
+        self.addSubview(self.view)
+        self.view.addSubview(self.pictureMovie)
+        self.view.addSubview(self.bottomView)
+        self.bottomView.addSubview(self.nameMovie)
+        
+        self.setupViewConstraints()
+        self.setupPictureMovieConstraints()
+        self.setupBottomViewConstraints()
+        self.setupNameMovieConstraints()
+    }
+    
+    private func setupViewConstraints() {
+        NSLayoutConstraint.activate([
+            self.view.topAnchor.constraint(equalTo: self.topAnchor),
+            self.view.leftAnchor.constraint(equalTo: self.leftAnchor),
+            self.view.rightAnchor.constraint(equalTo: self.rightAnchor),
+            self.view.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        self.view.backgroundColor = .green
+    }
+    
+    private func setupPictureMovieConstraints() {
+        NSLayoutConstraint.activate([
+            self.pictureMovie.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.pictureMovie.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            self.pictureMovie.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            self.pictureMovie.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.pictureMovie.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+        self.pictureMovie.backgroundColor = .orange
+    }
+
+    private func setupBottomViewConstraints() {
+        NSLayoutConstraint.activate([
+            self.bottomView.heightAnchor.constraint(equalToConstant: 40),
+            self.bottomView.widthAnchor.constraint(equalToConstant: self.frame.width),
+            self.bottomView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            self.bottomView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            self.bottomView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        self.bottomView.backgroundColor = .red
+    }
+    
+    private func setupNameMovieConstraints() {
+        NSLayoutConstraint.activate([
+//            self.nameMovie.centerXAnchor.constraint(equalTo: self.bottomView.centerXAnchor),
+            self.nameMovie.centerYAnchor.constraint(equalTo: self.bottomView.centerYAnchor),
+            self.nameMovie.leftAnchor.constraint(equalTo: self.bottomView.leftAnchor, constant: 12)
+        ])
+    }
+}
--- inchurch-ios-challenge/Controllers/Home/HomeViewController.swift
+++ inchurch-ios-challenge/Controllers/Home/HomeViewController.swift
@@ -13,16 +13,13 @@ class HomeViewController: UIViewController {
     private let collectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .black
+        collectionView.backgroundColor = .white
         return collectionView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupView()
-        
-        self.navigationController?.navigationBar.prefersLargeTitles = true
-        self.navigationController?.navigationItem.largeTitleDisplayMode = .never
     }
     
     private func setupView() {
@@ -30,6 +27,15 @@ class HomeViewController: UIViewController {
         self.view.backgroundColor = .white
         
         self.view.addSubview(self.collectionView)
+        
+        self.setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        self.collectionView.register(cellType: MovieCollectionViewCell.self)
         
         self.setupCollectionViewConstaints()
     }
@@ -41,6 +47,24 @@ class HomeViewController: UIViewController {
             self.collectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -12),
             self.collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
         ])
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.size.width == 414 ? 180 : 160,
+                      height: UIScreen.main.bounds.size.height == 896 ? 280 : 260)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(for: indexPath) as MovieCollectionViewCell
+        cell.bind()
+        cell.backgroundColor = .blue
+        return cell
     }
 }
 
