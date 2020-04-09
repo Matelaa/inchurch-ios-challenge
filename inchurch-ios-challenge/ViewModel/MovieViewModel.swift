27e54bc5a693760462f67ca8d1e4cd0cea2a5e58
Jose Matela <jmatelaneto@gmail.com>
2020-04-09 00:36:20 -0300

    creating a class to make the business rules

--- inchurch-ios-challenge/ViewModel/MovieViewModel.swift
+++ inchurch-ios-challenge/ViewModel/MovieViewModel.swift
@@ -7,3 +7,85 @@
 //
 
 import Foundation
+import RealmSwift
+
+struct MovieView {
+    
+    var id = 0
+    var title = ""
+    var overview = "Without overview"
+    var image = ""
+    var releaseDate = ""
+    var favorite = false
+    
+    var imageUrl: URL? {
+        return URL(string: imageBaseURL + self.image)
+    }
+}
+
+class MovieViewModel {
+    static func getAsView(movie: Movie?) -> MovieView {
+        guard let movie = movie else {
+            return MovieView()
+        }
+        
+        var movieView = MovieView()
+        movieView.id = movie.id.value ?? 0
+        movieView.title = movie.title ?? ""
+        movieView.image = movie.image ?? ""
+        movieView.releaseDate = movie.releaseDate ?? ""
+        movieView.favorite = movie.favorite.value ?? false
+        
+        if let overview = movie.overview, overview != "" {
+            movieView.overview = overview
+        }
+        
+        return movieView
+    }
+    
+    static func getAsView(sequence: [Movie]) -> [MovieView] {
+        var moviesView: [MovieView] = []
+        for movie in sequence {
+            moviesView.append(self.getAsView(movie: movie))
+        }
+        return moviesView
+    }
+    
+    static func saveAll(movies: [Movie]) {
+        for movie in movies {
+            let movieView = self.get(by: movie.id.value ?? -1)
+            movie.favorite.value = movieView.favorite
+        }
+        try! uiRealm.write {
+            uiRealm.add(movies, update: .all)
+        }
+    }
+    
+    static func getAll() -> [MovieView] {
+        return self.getAsView(sequence: self.get())
+    }
+    
+    static func deleteAll() {
+        let objects = self.get().filter { (movie) -> Bool in
+            if let favorite = movie.favorite.value {
+                return !favorite
+            }
+            return true
+        }
+        try! uiRealm.write {
+            uiRealm.delete(objects)
+        }
+    }
+    
+    static func get() -> [Movie] {
+        let objects = uiRealm.objects(Movie.self).sorted(byKeyPath: "title")
+        var movies: [Movie] = []
+        movies.append(contentsOf: objects)
+        return movies
+    }
+    
+    static func get(by id: Int) -> MovieView {
+        let object = uiRealm.object(ofType: Movie.self, forPrimaryKey: id)
+        return self.getAsView(movie: object)
+    }
+}
